0c7e7efc00d749193201aaeada24cb9d1aa98430
Lance <lance.jiexi@gmail.com>
2023-10-11 18:11:39 +0200

    added protectedProcedure

--- src/trpc/trpc.ts
+++ src/trpc/trpc.ts
@@ -1,3 +1,4 @@
+import supabaseServerComponentClient from "@/lib/supabaseServer";
 import { initTRPC } from "@trpc/server";
 
 /**
@@ -5,6 +6,25 @@ import { initTRPC } from "@trpc/server";
  * Should be done only once per backend!
  */
 const t = initTRPC.create();
+const middleware = t.middleware;
+
+const isAuth = t.middleware(async (opts) => {
+  const supabase = await supabaseServerComponentClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (!user) {
+    throw new Error("Unauthorized");
+  }
+
+  return opts.next({
+    ctx: {
+      userId: user.id,
+      user,
+    },
+  });
+});
 
 /**
  * Export reusable router and procedure helpers
@@ -12,3 +32,4 @@ const t = initTRPC.create();
  */
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const protectedProcedure = t.procedure;
