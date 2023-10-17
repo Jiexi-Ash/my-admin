90e4ca57fbca6a441ad0a0cbe7569d6c2e18c796
Lance <lance.jiexi@gmail.com>
2023-10-17 11:35:55 +0200

    added myFaculty page

---
+++ src/app/myfaculty/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function FacultyPage() {
+  return (
+    <div className="w-full min-h-screen px-10">
+      <div className="flex flex-col space-y-4 mt-10">
+        <div className="w-full flex justify-end">{/* add form */}</div>
+        {/* add table */}
+      </div>
+    </div>
+  );
+}
+
+export default FacultyPage;
